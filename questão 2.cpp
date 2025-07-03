7841a6b67a8fe6b1e9e79fdd969c0bdc53004dad
stheffany22 <sthecardoso22@gmail.com>
2025-07-03 08:15:27 -0300

    Create questão 2.cpp

---
+++ questão 2.cpp
@@ -0,0 +1,21 @@
+Algoritmo questão 2:
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int num1, num2;
+ cout << "informe um numero: ";
+ cin >> num1;
+ cout << "informe um numero: ";
+ cin >> num2;
+
+ cout << "Soma: " << num1 + num2 <<endl;
+ cout << "Subtracao: " << num1 - num2 <<endl;
+ cout << "Multiplicacao: " <<num1 * num2 <<endl;
+ cout << "Divisao: " <<num1 / num2 <<endl;
+
+ return 0;
+}
